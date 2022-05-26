a7329fad92fb204ef8dba1f9253f9d7d2dd29b01
Venkatesh <venkatesh.kamalapathy@analog.com>
2022-05-27 01:05:51 +0530

    Moving stuffs to openapispec parser

--- src/index.ts
+++ src/index.ts
@@ -11,67 +11,10 @@ export function main() {
     logger.info('Starting the parser!');
     const parser = new OpenApiParser(JSON.stringify(petstore));
     
-    // const response = parser.generateObjectFromSchema("#/components/schemas/Pet");
-    let count = 0;
+    const response = parser.generateObjectFromSchema("#/components/schemas/Pet");
+    console.log(`RESPONSE = ${JSON.stringify(response)}`);
 
-    let paths = [].concat(...getRefsPath(JSON.parse(JSON.stringify(petstore))));
-
-    console.log(`RESPONSE = ${paths.length}`);
-    while (paths.length > 0) {
-        paths.forEach(path => {
-            let p = (getJsonObject(path));
-            const json = parser.getJsonFromPath(p['$ref']);
-            delete p['$ref'];
-            setJson(path, JSON.stringify(json));
-            console.log('== ' + path + ' = ' + p + ' : ' + JSON.stringify(json));
-        });
-        paths = [].concat(...getRefsPath(JSON.parse(JSON.stringify(petstore))));
-    }
-    console.log('TEST=======' + JSON.stringify(petstore));
     return true;
-}
-
-function setJson(path: string, jsonString: string) {
-
-    let parentObjPath = path.substring(0, path.lastIndexOf(separator));
-
-    let leaf = path.substring(path.lastIndexOf(separator) + 1);
-    let parentObj = getJsonObject(parentObjPath);
-    parentObj[leaf] = JSON.parse(jsonString);
-    console.log(`UPDATED =  ${parentObjPath} & (${leaf})`);
-    // let p = getJsonObject(path);
-
-}
-
-
-function getJsonObject(path: string) {
-    if (!path) {
-        return;
-    }
-    const subPath = path.split(separator);
-    let tmp = petstore;
-    subPath.forEach(p => {
-        tmp = tmp[p];
-    });
-    return tmp;
-}
-
-function getRefsPath(json: JSON, path = []) {
-    let result = [];
-    Object.keys(json).forEach(function (prop) {
-        console.log(prop + '  ' + typeof json[prop] + " " + path);
-        if (prop == '$ref') {
-            console.log('TEST');
-            result.push(path.join(separator));
-        }
-        if (typeof json[prop] == 'object') {
-            const computedPath = getRefsPath(json[prop], [...path, prop]);
-            if (computedPath.length > 0)
-                result.push([].concat(...computedPath));
-        }
-        return result;
-    });
-    return result;
 }
 
 main();
--- src/open-api-parser.ts
+++ src/open-api-parser.ts
@@ -2,26 +2,50 @@ import { DataUtil } from "./util/data-util";
 import { log } from "./util/logger";
 import valueGeneratorFactory from "./value-generator-factory";
 
-const logger = log.getChildLogger({ name: 'OpenApiParser'}); 
+const logger = log.getChildLogger({ name: 'OpenApiParser'});
+const refSeparator = '"';
+
 export class OpenApiParser {
     openapispec: JSON;
+    isFlattened: boolean;
 
     constructor(openapispec: string) {
         this.openapispec = JSON.parse(openapispec);
+        this.isFlattened = false;
+    }
+
+    private flattenReferences(jsonString: string) {
+        let paths = [].concat(...OpenApiParser.getRefsPath(JSON.parse(JSON.stringify(this.openapispec))));
+
+        console.log(`RESPONSE = ${paths.length}`);
+        while (paths.length > 0) {
+            paths.forEach(path => {
+                let p = (this.getJsonFromPath(path, refSeparator));
+                const json = this.getJsonFromPath(p['$ref'], '/');
+                delete p['$ref'];
+                this.setJson(path, JSON.stringify(json));
+                console.log('== ' + path + ' = ' + p + ' : ' + JSON.stringify(json));
+            });
+            paths = [].concat(...OpenApiParser.getRefsPath(JSON.parse(JSON.stringify(this.openapispec))));
+        }
+        this.isFlattened = true;
+        console.log('TEST=======' + JSON.stringify(this.openapispec));
     }
 
     public generateObjectFromSchema(schemaPath: string) {
-        const jsonObject = this.getJsonFromPath(schemaPath);
+        if(!this.isFlattened) this.flattenReferences(JSON.stringify(this.openapispec));
+        const jsonObject = this.getJsonFromPath(schemaPath, '/');
         const generatedObject = valueGeneratorFactory.parseSingleSchemaElement(jsonObject);
         if(generatedObject instanceof Map) {
             return DataUtil.convertMapToObject(generatedObject);
         }
         return generatedObject;
     }
-    public getJsonFromPath(ref: string) {
+
+    public getJsonFromPath(ref: string, separator: string) {
         ref = ref.replace('#/','');
         logger.debug(`parsing ref ${ref}`);
-        const refSegments = ref.split('/');
+        const refSegments = ref.split(separator);
         let tmp = this.openapispec;
         for(let idx=0; idx<refSegments.length; idx++){
             tmp = tmp[refSegments[idx]];
@@ -29,4 +53,28 @@ export class OpenApiParser {
         return tmp;
     }
 
+    private setJson(path: string, jsonString: string) {
+        let parentObjPath = path.substring(0, path.lastIndexOf(refSeparator));
+        let leaf = path.substring(path.lastIndexOf(refSeparator) + 1);
+        let parentObj = this.getJsonFromPath(parentObjPath, refSeparator);
+        parentObj[leaf] = JSON.parse(jsonString);
+        console.log(`UPDATED =  ${parentObjPath} & (${leaf})`);
+    }
+
+    private static getRefsPath(json: JSON, path = []) {
+        let result = [];
+        Object.keys(json).forEach(function (prop) {
+            if (prop == '$ref') {
+                console.log('TEST');
+                result.push(path.join(refSeparator));
+            }
+            if (typeof json[prop] == 'object') {
+                const computedPath = OpenApiParser.getRefsPath(json[prop], [...path, prop]);
+                if (computedPath.length > 0)
+                    result.push([].concat(...computedPath));
+            }
+            return result;
+        });
+        return result;
+    }
 }
