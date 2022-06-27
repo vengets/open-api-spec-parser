d8e3d87ad8c6d3ea34d8b28c34d25be5dc4a217a
Venkatesh <venkatesh.kamalapathy@analog.com>
2022-06-27 22:32:04 +0530

    Clean up and npm related changes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import { log } from "./util/logger";
-import ps from "./data/pet-store.json";
-import factory from "./value-generator-factory";
-import { OpenApiParser } from "./open-api-parser";
+'use strict';
 
-const logger = log.getChildLogger({ name: 'main' });
-const separator = '"';
-let petstore = ps;
-
-export function main() {
-    logger.info('Starting the parser!');
-    const parser = new OpenApiParser(JSON.stringify(petstore));
-    
-    const response = parser.generateObjectFromSchema("#/definitions/Pet");
-    console.log(`RESPONSE = ${JSON.stringify(response)}`);
-
-    return true;
+module.exports = {
+    OpenApiParser: require('./open-api-parser')
 }
-
-main();
