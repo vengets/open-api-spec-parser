f33a72f91568f3b3f87c2cc4034e56a262c69522
Venkatesh <venkatesh.kamalapathy@analog.com>
2022-07-17 23:34:26 +0530

    Testing configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 'use strict';
+import ps from './data/pet-store.json';
+import { OpenApiParser } from './open-api-parser';
+
+function test() {
+    const parser = new OpenApiParser(JSON.stringify(ps));
+    const response = parser.generateObjectFromSchema("#/components/requestBodies/RegisterSimRequest");
+    console.log(`RESPONSE = ${JSON.stringify(response)}`);
+}
+
+test();
 
 module.exports = {
     OpenApiParser: require('./open-api-parser')
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,2 +1,3 @@
 import { Logger } from "tslog";
-export const log: Logger = new Logger({ name: "OPEN-API-SPEC-TESTER" });
+
+export const log: Logger = new Logger({ name: "OPEN-API-SPEC-TESTER", minLevel: 'warn' });
